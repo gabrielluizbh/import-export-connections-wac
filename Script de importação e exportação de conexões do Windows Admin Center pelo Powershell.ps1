60ac6b948696b9ddd736b89785994ab49501bae1
Gabriel Luiz <gabrielluiz@msn.com>
2021-01-05 21:14:03 -0300

    Add files via upload

---
+++ Script de importação e exportação de conexões do Windows Admin Center pelo Powershell.ps1
@@ -0,0 +1,44 @@
+﻿## Script de importação e exportação de conexões do Windows Admin Center - Créditos Gabriel Luiz - www.gabrielluiz.com ##
+
+# Libera a execução do script.
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+
+# Carregue o módulo.
+
+Import-Module "$env:ProgramFiles\windows admin center\PowerShell\Modules\ConnectionTools"
+
+
+# Importar conexões (incluindo tags) de um arquivo .csv.
+
+Import-Connection "https://wac2.gabrielluiz.local" -fileName "C:\WAC\Import-WAC-Conexoes.csv"
+
+# Exportar conexões (incluindo tags) para um arquivo .csv.
+
+Export-Connection "https://wac2.gabrielluiz.local" -fileName "C:\WAC\Export-WAC-Conexoes.csv"
+
+
+# Importe conexões (incluindo tags) de arquivos .csv e remova quaisquer conexões que não estejam explicitamente no arquivo importado usando o parâmetro de opção -prune.
+
+Import-Connection "https://wac2.gabrielluiz.local" -fileName "C:\WAC\Import-WAC-Conexoes.csv" -prune
+
+
+# Se estiver usando o Windows Admin Center instalado no Windows 10, modo local, utilize o exemplo abaixo, informe o endereço local seguido pela porta de acesso:
+
+
+# Importar conexões (incluindo tags) de um arquivo .csv modo de instalação local.
+
+Import-Connection "https://localhost:6516" -fileName "C:\WAC\Import-WAC-Conexoes.csv"
+
+
+Export-Connection "https://localhost:6516" -fileName "C:\WAC\Export-WAC-Conexoes.csv"
+
+
+<#
+
+Referência:
+
+https://docs.microsoft.com/pt-br/windows-server/manage/windows-admin-center/configure/use-powershell?WT.mc_id=WDIT-MVP-5003815
+
+#>
---
+++ Script de levantamento de computadores do ADDS.ps1
@@ -0,0 +1,51 @@
+﻿## Script de levantamento de informações do Active Directory - Objeto Computadores - Créditos Gabriel Luiz - www.gabrielluiz.com ##
+
+
+##################################################################  REQUISITOS  ######################################################################
+
+# Libera a execução do script.
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+
+# Importa o modulo AD.
+
+Import-Module ActiveDirectory
+
+
+##################################################################  CRIAÇÃO DAS PASTAS  ######################################################################
+
+
+# Cria as pasta para execução do levantamento de informações do Active Directory usando o nome do domínio.
+
+
+$nome=@(get-addomain | select-object forest).forest
+
+
+New-Item -Path "c:\Levantamento dos computadores do ADDS $nome" -ItemType directory
+
+
+################################################################## COMPUTADORES DO DOMÍNIO  ####################################################################
+
+# Todos os computadores.
+
+Get-ADComputer -Filter * -Properties Name, PasswordLastSet, OperatingSystem, DNSHostName, DistinguishedName, Enabled | select Name, PasswordLastSet, OperatingSystem, DNSHostName, DistinguishedName, Enabled | Export-Csv "c:\Levantamento dos computadores do ADDS $nome\todososcomputadores.csv" -Encoding UTF8
+
+
+<#
+
+Referências:
+
+https://docs.microsoft.com/en-us/powershell/module/addsadministration/get-adcomputer?view=win10-ps&WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-item?view=powershell-7.1&WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/import-module?view=powershell-7.1&WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/addsadministration/get-addomain?view=win10-ps&WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/select-object?view=powershell-7.1&WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/pt-br/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.1&WT.mc_id=WDIT-MVP-5003815
+
+#>
